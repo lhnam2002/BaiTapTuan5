427c3af32b38e38ea006010f698befb98e8ec1a9
lhnam2002 <luuhoangnam261202@gmail.com>
2023-09-23 20:24:20 +0700

    MaDiTuan, 8Hau

---
+++ BaiTap/8_Hau/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Hau
+{
+    class Program
+    {
+        static int N = 8;
+
+        static bool IsSafe(int[,] board, int row, int col)
+        {
+            int i, j;
+
+            for (i = 1; i <= col; i++)
+                if (board[row, i] == 1)
+                    return false;
+
+            for (i = row, j = col; i > 0 && j > 0; i--, j--)
+                if (board[i, j] == 1)
+                    return false;
+
+            for (i = row, j = col; j <= N && i > 0; i--, j++)
+                if (board[i, j] == 1)
+                    return false;
+
+            return true;
+        }
+
+        static bool SolveNQUtil(int[,] board, int col)
+        {
+            if (col > N)
+                return true;
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (IsSafe(board, i, col))
+                {
+                    board[i, col] = 1;
+
+                    if (SolveNQUtil(board, col + 1))
+                        return true;
+
+                    board[i, col] = 0; // Backtrack
+                }
+            }
+
+            return false;
+        }
+
+        static void PrintSolution(int[,] board)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                    Console.Write(board[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+
+        static bool SolveNQ(int startRow, int startCol)
+        {
+            int[,] board = new int[N + 1, N + 1];
+            board[startRow, startCol] = 1;
+
+            if (SolveNQUtil(board, startCol + 1) == false)
+            {
+                Console.WriteLine("Không có giải pháp");
+                return false;
+            }
+
+            PrintSolution(board);
+            return true;
+        }
+
+        static void Main()
+        {
+            int startRow = 1; // Hàng muốn đặt con hậu đầu tiên (từ 1 đến 8)
+            int startCol = 1; // Cột muốn đặt con hậu đầu tiên (từ 1 đến 8)
+
+            if (SolveNQ(startRow, startCol))
+                Console.WriteLine("Có giải pháp!");
+            else
+                Console.WriteLine("Không có giải pháp");
+            Console.Read();
+        }
+    }
+}
---
+++ BaiTap/MaDiTuan/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaDiTuan
+{
+    class Program
+    {
+        static int N = 8;
+        static int[] moveX = { 2, 1, -1, -2, -2, -1, 1, 2 };
+        static int[] moveY = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+        static bool IsSafe(int x, int y, int[,] sol)
+        {
+            return (x >= 1 && y >= 1 && x <= N && y <= N && sol[x, y] == -1);
+        }
+
+        static int CountMoves(int x, int y, int[,] sol)
+        {
+            int count = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                int nextX = x + moveX[i];
+                int nextY = y + moveY[i];
+
+                if (IsSafe(nextX, nextY, sol))
+                    count++;
+            }
+            return count;
+        }
+
+        static bool SolveKnightTour(int x, int y, int moveCount, int[,] sol)
+        {
+            if (moveCount == N * N)
+                return true;
+
+            int minMove = int.MaxValue;
+            int minIndex = -1;
+
+            for (int i = 0; i < 8; i++)
+            {
+                int nextX = x + moveX[i];
+                int nextY = y + moveY[i];
+
+                if (IsSafe(nextX, nextY, sol))
+                {
+                    int numMoves = CountMoves(nextX, nextY, sol);
+                    if (numMoves < minMove)
+                    {
+                        minMove = numMoves;
+                        minIndex = i;
+                    }
+                }
+            }
+
+            if (minIndex != -1)
+            {
+                int nextX = x + moveX[minIndex];
+                int nextY = y + moveY[minIndex];
+                sol[nextX, nextY] = moveCount;
+                if (SolveKnightTour(nextX, nextY, moveCount + 1, sol))
+                    return true;
+                else
+                    sol[nextX, nextY] = -1;
+            }
+
+            return false;
+        }
+
+        static void PrintSolution(int[,] sol)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= N; j++)
+                    Console.Write(sol[i, j].ToString().PadLeft(2) + " ");
+                Console.WriteLine();
+            }
+        }
+
+        static void Main()
+        {
+            int[,] sol = new int[N + 1, N + 1];
+            for (int i = 1; i <= N; i++)
+                for (int j = 1; j <= N; j++)
+                    sol[i, j] = -1;
+
+            int start_x = 3, start_y = 2;
+            sol[start_x, start_y] = 0;
+
+            if (SolveKnightTour(start_x, start_y, 1, sol))
+                PrintSolution(sol);
+            else
+                Console.WriteLine("Không có giải pháp");
+            Console.ReadLine();
+        }
+    }
+}
